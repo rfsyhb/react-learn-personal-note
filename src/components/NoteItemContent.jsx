54689d6bb0dbe5d1193d41f0f950828443e18102
rfsyhb <rafisyihab3@gmail.com>
2024-03-22 18:07:06 +0700

    note-item main

--- src/components/NoteItemContent.jsx
+++ src/components/NoteItemContent.jsx
@@ -5,7 +5,9 @@ function NoteItemContent({ title, body, date }) {
     <div className="note-item__content">
       <h3 className="note-item__title">{title}</h3>
       <p className="note-item__date">{date}</p>
-      <p className="note-item__body">{content}</p>
+      <p className="note-item__body">{body}</p>
     </div>
   );
 }
+
+export default NoteItemContent;
