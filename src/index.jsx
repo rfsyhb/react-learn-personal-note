0c798ed81f13bc05680d6fa96a12afdd02480b74
rfsyhb <rafisyihab3@gmail.com>
2024-03-22 19:24:34 +0700

    noteapp

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/App";
+import NoteApp from "./components/NoteApp";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<NoteApp />);
