a8823d74d0bcb2996dae67057833e7f9f98f4bd2
rfsyhb <rafisyihab3@gmail.com>
2024-03-23 00:53:21 +0700

    add limiter note title dan value forms

--- src/components/NoteInput.jsx
+++ src/components/NoteInput.jsx
@@ -11,9 +11,12 @@ class NoteInput extends React.Component {
   }
 
   onChangeTitleHandler = (event) => {
-    this.setState({
-      title: event.target.value
-    })
+    const newTitle = event.target.value;
+    if (newTitle.length <= 50){
+      this.setState({
+        title: newTitle,
+      });
+    }
   }
 
   onChangeBodyHandler = (event) => {
@@ -31,12 +34,18 @@ class NoteInput extends React.Component {
     })
   }
 
+  renderTitleCharLimit() {
+    const title = this.state.title;
+    const charLeft = 50 - title.length;
+    return charLeft === 0 ? 'Karakter sudah batas limit!' : `Karakter tersedia: ${charLeft}`;
+  }
+
   render() {
     return (
       <form className="note-input" onSubmit={this.onSubmitInputHandler}>
-        <input type="text" className="note-input__title" onChange={this.onChangeTitleHandler}/>
-        <p className="note-input__title-char-limit"></p>
-        <textarea type="text" className="note-input__body" onChange={this.onChangeBodyHandler}/>
+        <input type="text" className="note-input__title" value={this.state.title} onChange={this.onChangeTitleHandler}/>
+        <p className="note-input__title__char-limit">{this.renderTitleCharLimit()}</p>
+        <textarea type="text" className="note-input__body" value={this.state.body} onChange={this.onChangeBodyHandler}/>
         <button>Buat</button>
       </form>
     );
